f34b280d4f8864ac4149dfebc9feecc74d906be3
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2023-03-04 16:23:13 +0100

    :recycle: Clean and better version of Map 2D node

--- scripts/map2D.py
+++ scripts/map2D.py
@@ -1,23 +1,29 @@
 import argparse
-import get_OSM_data
 import generatePlane
 import logging
+import data2D
 
+# Parsing of all arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("--GPSFile", help="GPSFile", type=str)
-ap.add_argument("--dist", help="dist", type=str)
-ap.add_argument("--outputPath", help="output", type=str)
-ap.add_argument("--outputFolder", help="outputFolder", type=str)
+ap.add_argument("--GPSFile", help="GPS JSON file", type=str)
+ap.add_argument("--dist", help="distance from center point", type=str)
+ap.add_argument("--layersWanted", help="layers wanted for 2d map", type=str)
+ap.add_argument("--roadsName", help="boolean for name of roads", type=str)
+ap.add_argument("--outputPath", help="output file path", type=str)
+ap.add_argument("--outputFolder", help="output folder path", type=str)
 args = ap.parse_args()
 
 logging.basicConfig(level=logging.INFO)
 
 def main():
     logging.info("2D Map !")
-    image = get_OSM_data.OSM_picture(args.GPSFile, args.outputFolder, args.dist)
+
+    # Get image of Open Street Map with corresponding layers
+    image = data2D.Map2D(args.GPSFile, args.dist, args.outputFolder, args.layersWanted, args.roadsName)
     logging.info(f"Path of Image : {image}")
 
-    generatePlane.generatePlane(image, args.outputFolder, args.outputPath)
+    # Create plane with texture that has been created
+    generatePlane.generatePlane(image, args.outputFolder, args.outputPath, args.dist)
 
     logging.info("2D Map generated")
 
