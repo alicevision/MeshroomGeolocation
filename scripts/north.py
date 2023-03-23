2288148f467a179c6fd468850b1f358e4ea66b94
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2023-03-23 01:33:34 +0100

    :recycle: Clean North node

--- scripts/north.py
+++ scripts/north.py
@@ -1,20 +1,28 @@
-import argparse
-import get_OSM_data
+from argparse import ArgumentParser
+import data2D
 import generateNorth
 import logging
+import logLevel
 
-ap = argparse.ArgumentParser()
-ap.add_argument("--GPSFile", help="GPSFile", type=str)
-ap.add_argument("--outputPath", help="output", type=str)
-ap.add_argument("--outputFolder", help="outputFolder", type=str)
-args = ap.parse_args()
-
-logging.basicConfig(level=logging.INFO)
+# Parsing of all arguments
+def buildArgumentParser() -> ArgumentParser:
+    ap = ArgumentParser()
+    ap.add_argument("--GPSFile", help="GPSFile", type=str)
+    ap.add_argument("--outputPath", help="output", type=str)
+    ap.add_argument("--verboseLevel", help="verbose level for logging", type=str)
+    ap.add_argument("--outputFolder", help="outputFolder", type=str)
+    return ap
 
 def main():
+    ap = buildArgumentParser()
+    args = ap.parse_args()
+
+    logging.basicConfig(level=logLevel.textToLogLevel(args.verboseLevel))
+
     logging.info("North !")
-    image = get_OSM_data.OSM_picture(args.GPSFile, args.outputFolder, 550)
-    logging.info(f"Path of Image : {image}")
+    # get image to know where is the north
+    image = data2D.Map2D(args.GPSFile, args.outputFolder, 550)
+    logging.debug(f"Path of Image : {image}")
 
     generateNorth.generateNorth(image, args.outputFolder, args.output)
 
