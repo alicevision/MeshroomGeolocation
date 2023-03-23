9e943483456d9ae4a73e48d3f29647709dcbb851
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2023-03-23 01:51:52 +0100

    :recycle: Clean WeatherHDRI node

--- scripts/getTimeDataset.py
+++ scripts/getTimeDataset.py
@@ -3,7 +3,6 @@ from timezonefinder import TimezoneFinder
 from datetime import datetime
 from dateutil import tz
 
-#TODO json to dict
 def timeOfDataset(SFMData, GPSData):
     # Opening JSON file
     with open(SFMData, 'r') as inputfile:
@@ -14,6 +13,8 @@ def timeOfDataset(SFMData, GPSData):
 
     if "Exif:OffsetTimeOriginal" in json_object["views"][0]["metadata"]:
         offsetTime = json_object["views"][0]["metadata"]["Exif:OffsetTimeOriginal"]
+
+        # get offset time
         offsetTime = offsetTime.replace(":", ".")
         offsetTime = int(float(offsetTime))
     else:
@@ -23,31 +24,24 @@ def timeOfDataset(SFMData, GPSData):
             json_gps = json.load(GPSfile)
         
         tf = TimezoneFinder()
-
         timezone = tf.timezone_at(lng=json_gps["longitude"], lat=json_gps["latitude"])
 
         from_zone = tz.gettz('UTC')
         to_zone = tz.gettz(timezone)
         start_date = datetime.strptime(date, "%Y:%m:%d %H:%M:%S")
 
+        # if no offset time, get offset time from GPS coordinates
         start_date = start_date.replace(tzinfo = from_zone)
         local = start_date.astimezone(to_zone)
-
         local = [x for x in str(local.utcoffset()).split(':')]
-
         offsetTime = int(local[0])
         
     date = date.replace(" ", ":")
 
     date = [x for x in date.split(":")]
 
-    # Data to be written
-    output = {
+    # Data to return
+    return {
         "datetime": date,
         "offsetTime": offsetTime
     }
-
-    # Serializing json
-    json_object = json.dumps(output, indent=4)
-
-    return json_object
--- scripts/get_hdri.py
+++ scripts/get_hdri.py
@@ -1,39 +1,38 @@
+from pathlib import Path
 import re
-import json
-
 import logging
 import requests
 
 
 def getHDRI(WeatherData, output):
-    # log some messages
-    logging.info('This is an info message.')
-
     # get weather     
-    # Reading from json file
-    json_object = json.loads(WeatherData)
-
-    weather = json_object["weather condition"]
+    weather = WeatherData["weather condition"]
 
     #parse csv file
-    csv_path ='./lib/meshroom/nodes/external_files/hdri_list.csv'
-    csv = open(csv_path, "r")
+    csv_path ='../external_files/hdri_list.csv'
+    
+    # Get the path of the current file and the parent folder
+    currentFilePath = Path(__file__).absolute()
+    currentFileFolderPath = currentFilePath.parent
+
+    csv = open((currentFileFolderPath / csv_path).resolve(), "r")
     csv = csv.read()
 
-    lines = [x for x in csv.split('\n')]
-    lines = lines[:-1]
+    lines = csv.split('\n')[:-1]
 
+    # Get infos of all hdri
     path = r'^(\d*),(.*),(.*)$'
     result = [re.search(path, line) for line in lines]
     result = [x for x in result if x != None]
 
+    # Use regex to get the right hdri
     result = [result[i] for i in range(len(result)) if ((int)(result[i].group(1))) == weather]
 
     URL = result[0].group(3)
     response = requests.get(URL, stream=True)
 
     if response.status_code == 200:
-        logging.info("Download started")
+        logging.debug("Download started")
 
         total_size = int(response.headers.get('content-length', 0))
         block_size = 1024
@@ -43,7 +42,7 @@ def getHDRI(WeatherData, output):
             for data in response.iter_content(block_size):
                 wrote = wrote + len(data)
                 progress = wrote / total_size * 100
-                logging.info(f'Download Progress: {progress}%')
+                logging.debug(f'Download Progress: {progress}%')
                 out.write(data)
     else:
-        logging.info('Request failed: %d' % response.status_code)
+        logging.error('Request failed: %d' % response.status_code)
