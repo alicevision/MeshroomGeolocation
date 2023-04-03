d03ef5d79d738eb14ce50371fecc0601a82f4819
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2023-04-03 12:05:00 +0200

    :recycle: Clean on LAZ to LAS because it was not clear

--- scripts/LAZtoLAS.py
+++ scripts/LAZtoLAS.py
@@ -2,14 +2,14 @@ import laspy
 import os
 import logging
 
+def convert_laz_to_las(in_laz, out_las):
+    las = laspy.read(in_laz)
+    las = laspy.convert(las)
+    las.write(out_las) 
+
 def convert(LAZFolder, OutputFolder):
     try:
-        logging.debug('Running LAZ_to_LAS.py')
-        
-        def convert_laz_to_las(in_laz, out_las):
-            las = laspy.read(in_laz)
-            las = laspy.convert(las)
-            las.write(out_las)        
+        logging.debug('Running LAZ_to_LAS.py')       
         
         in_dir = LAZFolder
 
@@ -21,7 +21,7 @@ def convert(LAZFolder, OutputFolder):
                     
                     out_las = OutputFolder+"/"+inFile
                     out_las = out_las.replace('laz', 'las') 
-                    logging.debug('working on file: ',out_las)
+                    logging.debug(f'working on file: {out_las}')
                     convert_laz_to_las(in_laz, out_las)
                                 
         logging.info('Finished without errors - LAZ_to_LAS.py')
