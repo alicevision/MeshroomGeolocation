789d50ac9e9a662e229314dbbddd8f733fa0b426
Fabien Castan <fabcastan@gmail.com>
2023-04-07 17:53:30 +0200

    linux compatibility

--- scripts/unzip_archive.py
+++ scripts/unzip_archive.py
@@ -4,7 +4,8 @@ import shutil
 
 def unzip(ArchiveToUnzip, OutputFolder):
     # Convert the path to the right format
-    dir_unzip = ArchiveToUnzip.replace('/', '\\')
+    dir_unzip = ArchiveToUnzip
+    # dir_unzip = ArchiveToUnzip.replace('/', '\\')
 
     # Unzip the archive
     shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
